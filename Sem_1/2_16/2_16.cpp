c61840faddbcd709f9ae42d488dca24e4961ab8e
Roma-Petrov-666 <roma2oo7@yandex.ru>
2025-12-02 15:22:40 +0500

    Add files via upload

---
+++ Sem_1/2_16/2_16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int n, sum=0;
+    std::cin>>n;
+    
+    for (int i=1; i < n+1; i++) {
+        if (i%2 == 0) {
+            sum += i;
+        } else {
+            sum -= i;
+        }
+    }
+    
+    std::cout<<sum;
+ 
+    return 0;
+}
