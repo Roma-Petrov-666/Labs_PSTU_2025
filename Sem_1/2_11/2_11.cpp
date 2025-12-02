ec924083f68820432454da3a9e89304a237f2248
Roma-Petrov-666 <roma2oo7@yandex.ru>
2025-12-02 14:53:27 +0500

    Update 2_11.cpp

--- Sem_1/2_11/2_11.cpp
+++ Sem_1/2_11/2_11.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 
 int main() {
-    int n, a, sign=0;
+    int n, a;
+    bool plusFirst = false;
+    bool minusFirst = false;
     std::cin>>n;
 
     for (int i=0; i < n; i++) {
         std::cin>>a;
-        if (a > 0) {
-            sign = 1;
-            break;
-        } else if (a < 0) {
-            sign = -1;
-            break;
+        if (!plusFirst && !minusFirst) {
+            if (a > 0) {
+                plusFirst = true;
+            } else if (a < 0) {
+                minusFirst = true;
+            }
         }
     }
     
-    if (sign == 1) {
+    if (plusFirst) {
         std::cout<<"Первые положительные";
-    } else if (sign == -1) {
+    } else if (minusFirst) {
         std::cout<<"Первые отрицательные";
     } else {
         std::cout<<"Все 0";
     }
  
     return 0;
+
 }
