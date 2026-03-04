7c428ff7a383309997df5f8a3e6e9515963e1313
Roma-Petrov-666 <roma2oo7@yandex.ru>
2026-03-04 10:54:23 +0500

    Update Hanoi.cpp

--- Sem_2/Hanoi/Hanoi.cpp
+++ Sem_2/Hanoi/Hanoi.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void hanoi(int n, int start, int point, int tmp) {
 	if (n == 0) return;
 	hanoi(n - 1, start, tmp, point);
-	cout << start << " -> " << point << endl;
+	cout<<start<<" -> "<<point<<endl;
 	hanoi(n - 1, tmp, point, start);
 }
 
@@ -12,4 +12,5 @@ int main() {
 	hanoi(3, 1, 3, 2);
 
 }
+
 
