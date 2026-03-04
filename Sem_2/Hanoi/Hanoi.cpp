ce1bec990969e376b54d5f5d95fd7f6b1d1537ba
Roma-Petrov-666 <roma2oo7@yandex.ru>
2026-03-04 10:53:50 +0500

    Update Hanoi.cpp

--- Sem_2/Hanoi/Hanoi.cpp
+++ Sem_2/Hanoi/Hanoi.cpp
@@ -1,10 +1,10 @@
-#include iostream
+#include <iostream>
 using namespace std;
 
 void hanoi(int n, int start, int point, int tmp) {
 	if (n == 0) return;
 	hanoi(n - 1, start, tmp, point);
-	cout  start   =   point  endl;
+	cout << start << " -> " << point << endl;
 	hanoi(n - 1, tmp, point, start);
 }
 
@@ -12,3 +12,4 @@ int main() {
 	hanoi(3, 1, 3, 2);
 
 }
+
