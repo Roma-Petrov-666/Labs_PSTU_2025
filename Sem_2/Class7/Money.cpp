a55433b537b9eaa4a01998ab09e078f598a8bc06
Roma-Petrov-666 <roma2oo7@yandex.ru>
2026-05-04 09:16:42 +0500

    Update Money.cpp

--- Sem_2/Class7/Money.cpp
+++ Sem_2/Class7/Money.cpp
@@ -1,9 +1,10 @@
 #include "Money.h"
 
-Money::Money() : rub(0), kop(0) {}
+Money::Money(void) : rub(0), kop(0) {}
+
+Money::~Money(void) {}
 
 Money::Money(long rubles, int kopeks) : rub(rubles), kop(kopeks) {
-    // Нормализация копеек (диапазон 0..99)
     if (kop < 0 || kop > 99) {
         rub += kop / 100;
         kop %= 100;
@@ -16,14 +17,40 @@ Money::Money(long rubles, int kopeks) : rub(rubles), kop(kopeks) {
 
 Money::Money(const Money& other) : rub(other.rub), kop(other.kop) {}
 
-Money::~Money() {}
+void Money::Set_kop(int k) {
+    kop = k;
+    if (kop < 0 || kop > 99) {
+        rub += kop / 100;
+        kop %= 100;
+        if (kop < 0) {
+            kop += 100;
+            rub--;
+        }
+    }
+}
 
 Money& Money::operator=(const Money& other) {
-    if (this != &other) {
-        rub = other.rub;
-        kop = other.kop;
-    }
+    if (this == &other) return *this;
+    rub = other.rub;
+    kop = other.kop;
     return *this;
+}
+
+Money Money::operator+(const Money& other) const {
+    long total_kop = (rub * 100 + kop) + (other.rub * 100 + other.kop);
+    long new_rub = total_kop / 100;
+    int  new_kop = total_kop % 100;
+    return Money(new_rub, new_kop);
+}
+Money Money::operator-(const Money& other) const {
+    long total_kop = (rub * 100 + kop) - (other.rub * 100 + other.kop);
+    long new_rub = total_kop / 100;
+    int  new_kop = total_kop % 100;
+    if (new_kop < 0) {
+        new_kop += 100;
+        new_rub--;
+    }
+    return Money(new_rub, new_kop);
 }
 
 bool Money::operator==(const Money& other) const {
@@ -36,18 +63,16 @@ bool Money::operator!=(const Money& other) const {
 
 std::ostream& operator<<(std::ostream& out, const Money& m) {
     out << m.rub << ",";
-    // Добавляем ведущий ноль для копеек, если нужно
     if (m.kop < 10) out << "0";
     out << m.kop;
     return out;
 }
 
 std::istream& operator>>(std::istream& in, Money& m) {
-    std::cout << "Rubles: ";
+    std::cout << "rub: ";
     in >> m.rub;
-    std::cout << "Kopeks (0..99): ";
+    std::cout << "kop : ";
     in >> m.kop;
-    // Нормализация
     if (m.kop < 0 || m.kop > 99) {
         m.rub += m.kop / 100;
         m.kop %= 100;
