21306de8cf1de9e6f8b6798e0d83208d1fd9b1d0
Roma-Petrov-666 <roma2oo7@yandex.ru>
2026-05-04 09:15:31 +0500

    Update Money.h

--- Sem_2/Class7/Money.h
+++ Sem_2/Class7/Money.h
@@ -4,19 +4,29 @@
 
 class Money {
 public:
-    Money();                           // конструктор без параметров
-    Money(long rubles, int kopeks);    // конструктор с параметрами
-    Money(const Money& other);         // конструктор копирования
-    ~Money();
+    Money(void);
+    Money(long rubles, int kopeks);
+    Money(const Money& other);
+    ~Money(void);
 
-    // Оператор присваивания
+    long Get_rub() const { return rub; }
+    int  Get_kop() const { return kop; }
+
+    void Set_rub(long r) { rub = r; }
+    void Set_kop(int k);
+
+    // Операция присваивания
     Money& operator=(const Money& other);
 
-    // Операции сравнения (необходимы для объединения множеств)
+    // Арифметические операции
+    Money operator+(const Money& other) const;
+    Money operator-(const Money& other) const;
+
+    // Операции сравнения
     bool operator==(const Money& other) const;
     bool operator!=(const Money& other) const;
 
-    // Дружественные функции ввода-вывода
+    // Дружественные функции ввода/вывода
     friend std::ostream& operator<<(std::ostream& out, const Money& m);
     friend std::istream& operator>>(std::istream& in, Money& m);
 
